42ea6649d7e883fcad46a93c9279df80aafd8579
millywanderi <wanderinyambura5@gmail.com>
2026-04-23 22:21:17 +0300

    Runs before each test

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app, db, User, Book
+
+
+class TestLibraryApi(unittest.TestCase):
+
+    def SetUp(self):
+        """Runs before each test"""
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///:memory'
+        self.app = app.test_client()
+
+        with app.app_context():
+            db.create_all()
+
