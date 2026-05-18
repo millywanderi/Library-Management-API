c181166d5d345126178a6b37eab76ee9405f2b05
millywanderi <wanderinyambura5@gmail.com>
2026-05-18 10:41:50 +0300

    Added development config

--- config.py
+++ config.py
@@ -2,6 +2,12 @@
 
 import os
 
+class developmentConfig:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://millie:ciku2015@localhost/Library_Management_API"
+    Debug = True
+    CACHE_TYPE = "SimpleCache"
+
+
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI)
     CACHE_TYPE = "SimpleCache"
