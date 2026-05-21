92ff1e86d9fe7431078facbc0e9b9b3922e309ac
millywanderi <wanderinyambura5@gmail.com>
2026-05-21 11:06:16 +0300

    Added print error on database to check if it connects

--- config.py
+++ config.py
@@ -21,4 +21,8 @@ class TestingConfig:
 
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("Missing SQLALCHEMY_DATABASE_URI in environment variables")
+
     CACHE_TYPE = "SimpleCache"
